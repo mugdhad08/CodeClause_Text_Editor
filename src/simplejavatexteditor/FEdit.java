41cca35c236e9260d1394df797dd3baee57d1b97
mugdhad08 <92158361+mugdhad08@users.noreply.github.com>
2023-07-30 23:27:28 +0530

    Add files via upload

---
+++ src/simplejavatexteditor/FEdit.java
@@ -0,0 +1,17 @@
+/**
+ * @name        Simple Java NotePad
+ * @package     ph.notepad
+ * 
+ */
+
+package simplejavatexteditor;
+
+import javax.swing.JTextArea;
+
+public class FEdit {
+
+    public static void clear(JTextArea textArea) {
+        textArea.setText("");
+    }
+
+}
